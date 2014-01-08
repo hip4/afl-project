842526c66e99102ab09880005087fb14c47bdd05
Christian Buergi <burgc5@bfh.ch>
2014-01-08 16:45:14 +0100

    added Applet classes for new supported files

---
+++ applet/src/main/java/ch/m02/comet/afl/applet/EmptyApplet.java
@@ -0,0 +1,9 @@
+package ch.m02.comet.afl.applet;
+
+import ch.m02.comet.afl.parser.Empty;
+
+public class EmptyApplet extends Empty {
+
+	private static final long serialVersionUID = 1L;
+	
+}
---
+++ applet/src/main/java/ch/m02/comet/afl/applet/PolygonApplet.java
@@ -0,0 +1,9 @@
+package ch.m02.comet.afl.applet;
+
+import ch.m02.comet.afl.parser.Polygon;
+
+public class PolygonApplet extends Polygon {
+
+	private static final long serialVersionUID = 1L;
+	
+}
