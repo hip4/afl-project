eeef76cc1c24ee3cce34db2f1f72385fabf28ae4
Patrick Haring <harip1@bfh.ch>
2014-02-09 23:47:03 +0100

    final version

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ mkdir ./build/ch/m02/comet/afl/
 mkdir ./build/ch/m02/comet/afl/applet/
 mkdir ./build/ch/m02/comet/afl/parser/
 
-cp -R ./applet/target/classes/ch/m02/comet/afl/applet/ ./build/ch/m02/comet/afl/applet/
-cp -R ./parser/target/classes/ch/m02/comet/afl/parser/ ./build/ch/m02/comet/afl/parser/
-cp -R ./parser/target/generated-sources/logo/ch/m02/comet/afl/parser/ ./build/
+cp -R ./applet/target/classes/ch/m02/comet/afl/applet/*.class ./build/ch/m02/comet/afl/applet/
+cp -R ./parser/target/classes/ch/m02/comet/afl/parser/*.class ./build/ch/m02/comet/afl/parser/
+cp -R ./parser/target/generated-sources/logo/ch/m02/comet/afl/parser/*.html ./build/
+cp -R ./parser/target/generated-sources/logo/ch/m02/comet/afl/parser/*.java ./build/
 echo "done"
